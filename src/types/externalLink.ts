fdb0c26a23fa17385dbf4fd2803e423c4359a076
emzet93 <maciej.m.janawa@gmail.com>
2023-11-10 11:58:05 +0100

    extend external link types with name and placeholder

--- src/types/externalLink.ts
+++ src/types/externalLink.ts
@@ -1,6 +1,8 @@
 export type Website = {
   id: WebsiteType
   profileRegex: string
+  name: string
+  placeholder: string
 }
 
 export type ExternalLinkUser = {
@@ -34,77 +36,113 @@ export const SupportedWebsites: Website[] = [
   {
     id: 'twitter',
     profileRegex: '^https?://(?:www.)?(?:twitter.com|x.com)/([a-zA-Z0-9_]+)/?$',
+    name: 'X',
+    placeholder: 'https://x.com/',
   },
   {
     id: 'instagram',
     profileRegex: '^https?://(?:www.)?instagram.com/([a-zA-Z0-9_.]+)/?$',
+    name: 'Instagram',
+    placeholder: 'https://instagram.com/',
   },
   {
     id: 'facebook',
     profileRegex: '^https?://(?:www.)?facebook.com/([a-zA-Z0-9.]+)/?$',
+    name: 'Facebook',
+    placeholder: 'https://facebook.com/',
   },
   {
     id: 'youtube',
     profileRegex: '^https?://(?:www.)?youtube.com/(?:c/)?([a-zA-Z0-9_-]+)/?$',
+    name: 'Youtube',
+    placeholder: 'https://youtube.com/',
   },
   {
     id: 'soundcloud',
     profileRegex: '^https?://(?:www.)?soundcloud.com/([a-zA-Z0-9_-]+)/?$',
+    name: 'SoundCloud',
+    placeholder: 'https://soundcloud.com/',
   },
   {
     id: 'tiktok',
     profileRegex: '^https?://(?:www.)?tiktok.com/@([a-zA-Z0-9_.-]+)/?$',
+    name: 'TikTok',
+    placeholder: 'https://tiktok.com/',
   },
   {
     id: 'twitch',
     profileRegex: '^https?://(?:www.)?twitch.tv/([a-zA-Z0-9_]+)/?$',
+    name: 'Twitch',
+    placeholder: 'https://twitch.tv/',
   },
   {
     id: 'discord',
     profileRegex: '^(?:https?://)?discord.gg(?:/invite)?/([a-zA-Z0-9-]+)/?$',
+    name: 'Discord',
+    placeholder: 'https://discord.com/',
   },
   {
     id: 'bandcamp',
     profileRegex: '^https?://(?:www.)?bandcamp.com/(?:[a-zA-Z0-9_-]+)/?$',
+    name: 'bandcamp',
+    placeholder: 'https://bandcamp.com/',
   },
   {
     id: 'itunes',
     profileRegex:
       '^https?://music.apple.com/(?:us/)?artist/([a-zA-Z0-9_-]+)/?$',
+    name: 'Apple Music',
+    placeholder: 'https://music.apple.com/',
   },
   {
     id: 'spotify',
     profileRegex:
       '^https?://open.spotify.com/(?:artist/|user/)?([a-zA-Z0-9_-]+)/?$',
+    name: 'Spotify',
+    placeholder: 'https://spotify.com/',
   },
   {
     id: 'tidal',
     profileRegex:
       '^https?://(?:www.)?tidal.com/(?:browse/artist/|artist/)([a-zA-Z0-9_-]+)/?$',
+    name: 'Tidal',
+    placeholder: 'https://tidal.com/',
   },
   {
     id: 'catalog',
     profileRegex: '^https?://(www.)?catalog.works/[a-zA-Z0-9_]+$',
+    name: 'Catalog',
+    placeholder: 'https://catalog.works/',
   },
   {
     id: 'sound',
     profileRegex: '^https?://(www.)?sound.xyz/[a-zA-Z0-9_]+$',
+    name: 'Sound',
+    placeholder: 'https://sound.xyz/',
   },
   {
     id: 'nina',
     profileRegex: '^https?://(www.)?ninaprotocol.com/[a-zA-Z0-9_]+$',
+    name: 'nina',
+    placeholder: 'https://ninaprotocol.com/',
   },
   {
     id: 'noizd',
     profileRegex: 'https?://(www.)?noizd.com/u/[a-zA-Z0-9_]+$/',
+    name: 'NOIZD',
+    placeholder: 'https://noizd.com/',
   },
   {
     id: 'hey',
     profileRegex: '^https?://(www.)?hey.xyz/u/[a-zA-Z0-9_]+$/',
+    name: 'Hey',
+    placeholder: 'https://hey.xyz/',
   },
   {
     id: 'zora',
     profileRegex: '^https?://(www.)?zora.co/[a-zA-Z0-9_]+$',
+    name: 'Zora',
+    placeholder: 'https://zora.co/',
   },
 ]
 
@@ -122,3 +160,9 @@ export const getSoundUrlFromHandle = (handle: string): string => {
   return `https://www.sound.xyz/${handle}`
 }
 
+export const deriveTwitterHandleFromUrl = (url: string) => {
+  const regex =
+    /(?:https?:\/\/)?(?:www\.)?(?:twitter\.com|x\.com)\/(?:#!\/)?@?([^\/\?]+)/;
+
+  return url.match(regex)?.[1];
+};
